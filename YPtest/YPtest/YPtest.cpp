be41707b32586b3cc3c31c348c86c6e6a3c316c4
ichi-k <k.ichikawa.mh@gmail.com>
2016-08-21 13:41:05 +0900

    制御関数を変更

--- YPtest/YPtest/YPtest.cpp
+++ YPtest/YPtest/YPtest.cpp
@@ -37,10 +37,10 @@ int main(void){
 	}
 
 
-	Spur_set_vel(0.2);
-	Spur_set_accel(1.0);
-	Spur_set_angvel(PI / 2.0);
-	Spur_set_angaccel(PI / 2.0);
+	Spur_set_vel(0.3);		//速度0.3m/sec
+	Spur_set_accel(1.0);	//加速度（m/s/s）
+	Spur_set_angvel(1.5);	//角速度（rad/s)
+	Spur_set_angaccel(2.0);		//角加速度（rad/s/s)
 
 	YP_set_wheel_vel(PI / 2.0, PI / 2.0);
 	YP_set_wheel_accel(PI / 2.0,PI / 2.0);
@@ -54,17 +54,26 @@ int main(void){
 			case ' ':	//スペースで停止
 				Spur_stop();
 				break;
-			case 0x48:  //↑で前進
-				YP_wheel_vel(vel, vel);
+			case 'y':  //↑で前進
+				//YP_wheel_vel(vel, vel);
+				Spur_line_GL(0, 0, 0);
 				break;
-			case 0x50:	//↓で後退
+		/*	case 'j':	//↓で後退
 				YP_wheel_vel(-vel, -vel);
-				break;
-			case 0x4b:	//←で左回転
-				YP_wheel_ang(-TREAD*(ang / 360) / TIRE, TREAD*(ang / 360) / TIRE);
+				break;*/
+			case 'g':	//←で左回転
+				//YP_wheel_ang(-TREAD*(ang / 360) / TIRE, TREAD*(ang / 360) / TIRE);
+				Spur_spin_GL(3.14 / 2);
+				while (!Spur_near_ang_GL(ang*3.14 / 180, 0.1))
+					Sleep(100);
+				Spur_stop();
 				break;
 			case 0x4d: //→で右回転
-				YP_wheel_ang(TREAD*(ang / 360) / TIRE, -TREAD*(ang / 360) / TIRE);
+				//YP_wheel_ang(TREAD*(ang / 360) / TIRE, -TREAD*(ang / 360) / TIRE);
+				Spur_spin_GL(-3.14 / 2);
+				while (!Spur_near_ang_GL(-ang*3.14 / 180, 0.1))
+					Sleep(100);
+				Spur_stop();
 				break;
 			case 'v':	//速度変更
 				printf("\033[2J");
